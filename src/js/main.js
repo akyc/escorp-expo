965d5ff01674f67f05349b4ab485d81e8c6c6002
Andrew Zyuzin <akyc@users.noreply.github.com>
2022-09-16 14:01:50 +0300

    .

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
 import * as bootstrap from 'bootstrap'
+
+let images = document.querySelectorAll('img')
+for (let image of images) {
+    image.onerror = function () {
+        if (/(eurosvet.info)/.test(image.src)) {
+            image.src = './images/' + image.src.replace(/(https:\/\/eurosvet.info\/catalog\/images\/products\/preview\/1)|(_0001)/gi, '')
+        }
+    }
+}
