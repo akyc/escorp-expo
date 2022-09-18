2f38a4b784ba2f6cf05e9db148c4a14261defb87
Andrew Zyuzin <akyc@users.noreply.github.com>
2022-09-18 11:19:58 +0300

    .

---
+++ src/data.js
@@ -0,0 +1,46 @@
+'a058494': 1,'a053081': 1,'a050335': 1,'a050115': 1,'a052740': 1,'a058979': 1,'a043954': 1,'a038823': 1,'a056311': 1,'a055774': 1
+
+
+'a058495': 1, 'a053083': 1, 'a051740': 1, 'a050117': 1, 'a052742': 1, 'a058982': 2, 'a043953': 1,'a055777': 1,'a055775': 1 
+
+
+'a053079': 1,'a053078': 1,'a050336': 1,'a050116': 1,'a05274': 1, 'a047883':1,
+
+[
+    {'c': '', 'rs': null, 'cs': null, 'n': null},
+]
+
+[
+    {'c': 'a053079', 'rs': null, 'cs': null, 'n': null},
+    {'c': 'a053078', 'rs': null, 'cs': null, 'n': null},
+    {'c': 'a050336', 'rs': null, 'cs': null, 'n': null},
+    {'c': 'a050116', 'rs': null, 'cs': null, 'n': null},
+    {'c': 'a052741', 'rs': null, 'cs': null, 'n': null},
+    {'c': 'a047883', 'rs': null, 'cs': null, 'n': null},
+    {'c': 'a055776', 'rs': null, 'cs': 2, 'n': null},
+    {'c': 'a055772', 'rs': null, 'cs': null, 'n': null},
+]
+
+"editor.renderWhitespace": "all",
+[{'c': 'a047871', 'rs': null, 'cs': null, 'n': null},{'c': 'a047855', 'rs': null, 'cs': null, 'n': null},{'c': 'a047873', 'rs': null, 'cs': null, 'n': null},{'c': 'a047874', 'rs': null, 'cs': null, 'n': null},{'c': 'a047872', 'rs': null, 'cs': null, 'n': null},{'c': 'a058986', 'rs': null, 'cs': null, 'n': null},{'c': 'a047884', 'rs': null, 'cs': null, 'n': null},{'c': 'a043955', 'rs': null, 'cs': null, 'n': null},{'c': 'a038440', 'rs': null, 'cs': null, 'n': null},{'c': 'a055773', 'rs': null, 'cs': null, 'n': null}]
+
+[{'c': 'a058494', 'rs': null, 'cs': null, 'n': null},{'c': 'a053081', 'rs': null, 'cs': null, 'n': null},{'c': 'a050335', 'rs': null, 'cs': null, 'n': null},{'c': 'a050115', 'rs': null, 'cs': null, 'n': null},{'c': 'a052740', 'rs': null, 'cs': null, 'n': null},{'c': 'a058979', 'rs': null, 'cs': null, 'n': null},{'c': 'a043954', 'rs': null, 'cs': null, 'n': null},{'c': 'a038823', 'rs': null, 'cs': null, 'n': null},{'c': 'a056311', 'rs': null, 'cs': null, 'n': null},{'c': 'a055774', 'rs': null, 'cs': null, 'n': null}]
+
+[{'c': 'a058495', 'rs': null, 'cs': null, 'n': null},{'c': 'a053083', 'rs': null, 'cs': null, 'n': null},{'c': 'a051740', 'rs': null, 'cs': null, 'n': null},{'c': 'a050117', 'rs': null, 'cs': null, 'n': null},{'c': 'a052742', 'rs': null, 'cs': null, 'n': null},{'c': 'a058982', 'rs': 2, 'cs': null, 'n': null},{'c': 'a043953', 'rs': null, 'cs': null, 'n': null},{'c': 'a055777', 'rs': null, 'cs': 2, 'n': null},{'c': 'a055775', 'rs': null, 'cs': null, 'n': null}]
+
+[{'c': 'a057589', 'rs': 2, 'cs': null, 'n': null},{'c': 'a057590', 'rs': 2, 'cs': null, 'n': null},{'c': 'a057583', 'rs': 2, 'cs': null, 'n': null},{'c': 'a057584', 'rs': 2, 'cs': null, 'n': null},{'c': 'a058287', 'rs': null, 'cs': null, 'n': null},{'c': 'a039164', 'rs': null, 'cs': null, 'n': null},{'c': 'a056594', 'rs': null, 'cs': null, 'n': null},{'c': 'a055628', 'rs': null, 'cs': null, 'n': null}]
+
+
+[{'c': 'a058286', 'rs': null, 'cs': 2, 'n': null},{'c': 'a052871', 'rs': null, 'cs': null, 'n': null},{'c': 'a056599', 'rs': null, 'cs': null, 'n': null},{'c': 'a055629', 'rs': null, 'cs': null, 'n': null}]
+
+[{'c': 'a060855', 'rs': 2, 'cs': null, 'n': null},{'c': 'a043958', 'rs': null, 'cs': null, 'n': null},{'c': 'a055361', 'rs': null, 'cs': null, 'n': null},{'c': 'a055362', 'rs': null, 'cs': null, 'n': null},{'c': 'a055428', 'rs': null, 'cs': null, 'n': null},{'c': 'a055427', 'rs': null, 'cs': null, 'n': null},{'c': 'a040512', 'rs': null, 'cs': null, 'n': null},{'c': 'a038372', 'rs': null, 'cs': 2, 'n': null}]
+
+[{'c': 'a043957', 'rs': null, 'cs': null, 'n': null},{'c': 'a058942', 'rs': null, 'cs': null, 'n': null},{'c': 'a060540', 'rs': null, 'cs': null, 'n': null},{'c': 'a058939', 'rs': null, 'cs': null, 'n': null},{'c': 'a039998', 'rs': null, 'cs': null, 'n': null},{'c': 'a039169', 'rs': null, 'cs': null, 'n': null},{'c': 'a037485', 'rs': null, 'cs': null, 'n': null}]
+
+[{'c': 'a059887', 'rs': null, 'cs': null, 'n': null},{'c': 'a055958', 'rs': null, 'cs': null, 'n': null},{'c': 'a058953', 'rs': null, 'cs': null, 'n': null},{'c': 'a058954', 'rs': null, 'cs': null, 'n': null},{'c': 'a061328', 'rs': null, 'cs': null, 'n': null},{'c': 'a061423', 'rs': null, 'cs': null, 'n': null},{'c': 'a061424', 'rs': null, 'cs': null, 'n': null},{'c': 'a061425', 'rs': null, 'cs': null, 'n': null},{'c': 'a061426', 'rs': null, 'cs': null, 'n': null},{'c': 'Pacific_NEW', 'rs': null, 'cs': null, 'n': null},{'c': 'Candle_NEW', 'rs': null, 'cs': null, 'n': null}]
+a059252 a059253 a061288 a061286 a057450 a057452 a057453 a060827 a060828 a060834 a057154
+
+a058469 a058080 a057335 a057336 a058632 a058634 a058636 a058637 a058642 a056035 a056838 a060675 a060673 a057473
+
+a055956 a058066 a057391 a057459 a058956 a058957 a061720 a060700 a060522 a056847 a058968 a058971 a058966 a058965
+a059258 a059262 a058076 a060588 a058498 a058499 a058500 a057462 a058962 a058963
